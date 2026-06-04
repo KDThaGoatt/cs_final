19fc64aef6ff536380a5a496358005753564de9b
KDThaGoatt <kyandupuis@gmail.com>
2026-06-04 10:18:49 -0700

    Finished datatype information page and began fixing the for loop in the row creation screen

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ class gui:
 
 
     def get_values(self):
+        self.viewedData = False
         self.table_name = self.name_entry.get()
         try:
             self.row_amount = int(self.rows_entry.get())
@@ -46,30 +47,76 @@ class gui:
     def row_creation(self):
         self.table_create.pack_forget()
         row_list = []
+        if self.viewedData == True:
+            self.datatypes.pack_forget()
+            self.viewedData = False
         for i in range(self.row_amount):
+            button1.config(state="disabled")
             if i != 0:
                 self.row_create.pack_forget()
             self.row_create = tk.Frame(self.window)
 
             label1 = tk.Label(self.row_create, text="Enter the title of your row, followed by the datatype in the format: title, datatype")
+            label2 = tk.Label(self.row_create, text=f"Row {i}")
 
+
+            entry1 = tk.Entry(self.row_create, width=20)
+
+            button1 = tk.Button(self.row_create, text="View Datatypes", width=20,height=5,anchor="center", command=self.datatype_screen)
+            
+
+            label1.grid(row=0, column=0, columnspan=3, sticky="n", pady=10)
+            label2.grid(row=2, column=0, columnspan=3, sticky="n", pady=10)
+
+            entry1.grid(row=3,column=0,columnspan=3,sticky="n",pady=10)
+
+            button1.grid(row=1, column=0, columnspan=3, sticky="n", pady=10)
+
+            self.row_create.pack()
+
+            self.row_create.wait_variable()
+    
+    def datatype_screen(self):
+            self.row_create.pack_forget()
+
+            self.datatypes = tk.Frame(self.window)
+            
+            label1 = tk.Label(self.datatypes, text="Datatypes:")
+            label2 = tk.Label(self.datatypes, text="INT / BIGINT: Whole numbers")
+            label3 = tk.Label(self.datatypes, text="DECIMAL / NUMERIC: Fixed point fractional numbers (exact)")
+            label4 = tk.Label(self.datatypes, text="FLOAT / REAL: Floating point fraction numbers (inexact)")
+            label5 = tk.Label(self.datatypes, text="CHAR(n): Fixed-length text strings, input the length you need as an integer for n")
+            label6 = tk.Label(self.datatypes, text="VARCHAR(n): Variable-length text strings, input the length you need as an integer for n")
+            label7 = tk.Label(self.datatypes, text="TEXT / LONGTEXT: Extremely large unstructured text blocks")
+            label8 = tk.Label(self.datatypes, text="DATE: Calendar dates without time")
+            label9 = tk.Label(self.datatypes, text="TIME: Duration or hour markers without date")
+            label10 = tk.Label(self.datatypes, text="DATETIME / TIMESTAMP: Calendar date aswell as clock time")
+            label11 = tk.Label(self.datatypes, text="DATETIMEOFFSET: Tracks point in time entries combined with explicit time zone offsets")
+            label12 = tk.Label(self.datatypes, text="BOOLEAN / BIT: Strict logical storage holding a true or false state")
+            label13 = tk.Label(self.datatypes, text="JSON: Semi-structured text column that natively validates JavaScript Object Notation syntax")
+            label13 = tk.Label(self.datatypes, text="VARBINARY(MAX) / BLOB: Stores raw unformatted binary byte streams")
+
+            button1 = tk.Button(self.datatypes, text="Return", width=20,height=5,anchor="center", command=self.row_creation)
+
+            self.viewedData=True
 
             label1.grid(row=0, column=0, columnspan=3, sticky="n", pady=10)
             label2.grid(row=1, column=0, columnspan=3, sticky="n", pady=10)
+            label3.grid(row=2, column=0, columnspan=3, sticky="n", pady=10)
+            label4.grid(row=3, column=0, columnspan=3, sticky="n", pady=10)
+            label5.grid(row=4, column=0, columnspan=3, sticky="n", pady=10)
+            label6.grid(row=5, column=0, columnspan=3, sticky="n", pady=10)
+            label7.grid(row=6, column=0, columnspan=3, sticky="n", pady=10)
+            label8.grid(row=7, column=0, columnspan=3, sticky="n", pady=10)
+            label9.grid(row=8, column=0, columnspan=3, sticky="n", pady=10)
+            label10.grid(row=9, column=0, columnspan=3, sticky="n", pady=10)
+            label11.grid(row=10, column=0, columnspan=3, sticky="n", pady=10)
+            label12.grid(row=11, column=0, columnspan=3, sticky="n", pady=10)
+            label13.grid(row=12, column=0, columnspan=3, sticky="n", pady=10)
 
-            self.row_create.pack()
-    
-    def datatype_screen(self):
-            
-            label1 = tk.Label(self.row_create, text="Datatypes:")
-            label2 = tk.Label(self.row_create, text="INT / BIGINT: Whole numbers")
-            label3 = tk.Label(self.row_create, text="DECIMAL / NUMERIC: Fixed point fractional numbers (exact)")
-            label4 = tk.Label(self.row_create, text="FLOAT / REAL: Floating point fraction numbers (inexact)")
-            label5 = tk.Label(self.row_create, text="CHAR(n): Fixed-length text strings, input the length you need as an integer for n")
-            label6 = tk.Label(self.row_create, text="VARCHAR(n): Variable-length text strings, input the length you need as an integer for n")
-            label7 = tk.Label(self.row_create, text="TEXT / LONGTEXT: Extremely large unstructured text blocks")
-            label8 = tk.Label(self.row_create, text="DATE: Calendar dates without time")
-            label9 = tk.Label(self.row_create, text="TIME: Duration or hour markers without date")
+            button1.grid(row=14, column=0, columnspan=3, sticky="n", pady=10)
+
+            self.datatypes.pack()
             
             
 
